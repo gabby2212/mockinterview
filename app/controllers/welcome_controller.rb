49ab5bcdc41a76f7ca96b4c8d2dfa45a9224bb52
Gaby Melchior <gdm2118@columbia.edu>
2013-08-06 12:10:42 -0400

    Fixed routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def show
+  	redirect_to '/users/sign_up'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Mockinterviewinit::Application.routes.draw do
-  get "welcome/index"
-
+  resources :welcome
   devise_for :users
 
 
@@ -53,11 +52,11 @@ Mockinterviewinit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'welcome#show'
+end
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
