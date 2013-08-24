b58d14c5dacbd7ead91d93fd6d96dedbedd9e16b
Gaby Melchior <gdm2118@columbia.edu>
2013-08-24 19:28:16 -0400

    Deleted serializer

--- app/serializers/student_serializer.rb
+++
@@ -1,7 +0,0 @@
-class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :matches, :first_name, :last_name, :school, :degree, :year_of_graduation, :major, :interviewing_for, :interview_type, :user_id
-
-  def matches
-  	object.find_connections
-  end
-end
